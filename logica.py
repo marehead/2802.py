3e7ef02312cdb180599a9dec54b3ce5c0804c56e
mlopes28 <marisanntl@hotmail.com>
2022-04-03 18:08:26 -0300

    second commit

---
+++ logica.py
@@ -0,0 +1,16 @@
+A = 20
+B = 20
+C = 22
+D = 15
+E = [10,20]
+F = [10,20]
+
+print(A is B)
+print(C is B)
+print(E is F)
+print(E is not F)
+
+#operado rde associacao
+l = [20,23,26,30]
+print(21 in l)
+
